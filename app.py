b77bd19ef759886efdee89749b11aec2a5c401b3
K-Tiva <k.chiba.mail@gmail.com>
2018-09-04 00:07:44 +0900

    Bug fix app.py in from linebot.exceptions

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
 )
-from linebot.execptions import (
+from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
